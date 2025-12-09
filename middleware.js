f9064d15f65992b7a95db7e345065298f7f8733b
Neeraja824 <155799322+Neeraja824@users.noreply.github.com>
2025-12-09 19:21:20 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,19 +1,29 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("api success");
 });
+
 app.put('/edit-user', (req, res) => {
     const mydata = req.body;
     console.log(mydata);
     res.send("data edited successfully");
 });
+
 app.post('/add-user', (req, res) => {
     const data = req.body;
     console.log(data);
     res.send("Data added");
 });
+
+app.delete('/delete-user', (req, res) => {
+    const deleteData = req.body;
+    console.log(deleteData);
+    res.send("User deleted");
+});
+
 app.listen(7000, () => {
     console.log("Server running at port 7000");
 });
