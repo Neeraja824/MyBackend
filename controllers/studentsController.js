3787d0e5b7956af749a6bd0d19dc95768a7a2b08
Neeraja824 <lankaneeraja824@gmail.com>
2025-12-26 19:07:54 +0530

    first

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -11,18 +11,44 @@
 
 // export {getStudents, addStudents};
 
-import student from "../models/studentsModels.js";
+// import student from "../models/studentsModels.js";
+// const getStudents = (req, res) => {
+//     const mydata = {name: "thub", roll: "1234"}; //db
+//     res.send(mydata);
+// };
+// const addStudents = async(req, res) => {
+//     try{
+//          const data = req.body
+//     console.log(data);
+//     const addeddata = await student.create(data);
+//     // const addeddata = await student.findOne({stdRoll: 1209});
+//     const mydata=await student.findById({});
+//     console.log(addeddata);
+//     res.status(201).json("data added");
+//     }catch(error){
+//         res.status(500).json({error: error.message})
+//     }
+// };
+// export {getStudents, addStudents};
 
-const getStudents = (req, res) => {
-    const mydata = {name: "thub", roll: "1234"}; //db
-    res.send(mydata);
+import student from "../models/studentsModels.js";
+const getStudents = async(req, res) => {
+    try{
+            // const mydata = await student.find();
+            // const mydata = await student.findOne();
+            const mydata = await student.findById({_id: "694b82e0c80115abff46e516"})
+            res.status(200).json(mydata);
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error: error.message})
+    }
 };
 const addStudents = async(req, res) => {
     try{
          const data = req.body;
     console.log(data);
     // const addeddata = await student.create(data);
-    const addeddata = await student.findOne({stdRoll: 1209});
+    const addeddata = await student.insertMany(data);
     console.log(addeddata);
     res.status(201).json("data added");
     }catch(error){
